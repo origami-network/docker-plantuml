644998a18e74060b8a7cc6040347d79ca95104d2
Bart Dubois <dubcio@o2.pl>
2018-07-24 00:06:39 +0200

    Fix publishing test results

--- Add-AppveyorTestResult.ps1
+++ Add-AppveyorTestResult.ps1
@@ -7,8 +7,6 @@ param(
     [string] $Framework = 'NUnit'
 )
 
-$ErrorActionPreference = 'Stop'
-
 
 begin {
     $uploadUrl = "https://ci.appveyor.com/api/testresults/$($Framework.ToLoower())/$($env:APPVEYOR_JOB_ID)"

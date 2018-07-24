2faa2a4d539041c302e90b93d24ebf73477fac57
Bart Dubois <dubcio@o2.pl>
2018-07-24 03:16:50 +0200

    Publising docker image CI/CD step implementation

--- Publish-DockerImage.ps1
+++ Publish-DockerImage.ps1
@@ -4,8 +4,8 @@
 param(
     [string] $BasePath = (Get-Location),
 
-    [hashtable] $Name = (. (Join-Path $BasePath 'project.config.ps1')).Image,
-    [string[]] $Tag = 'latest',
+    [string] $Name = (. (Join-Path $BasePath 'project.config.ps1')).Image,
+    [string[]] $Tag,
 
     [string] $UserName = $env:DOCKER_USERNAME,
     [string] $UserPassword = $env:DOCKER_USERPASSWORD
@@ -14,7 +14,73 @@ param(
 $ErrorActionPreference = 'Stop'
 
 
-Write-Error 'FIXME: [#6 5.] publish image'
-# Invoke-RestMethod https://hub.docker.com/v2/repositories/{name}/tags/?page=1 fo test image exists till next atribute will be empty or tag ware found
-# - docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
-# - docker push me/myfavoriteapp
+Write-Verbose "Docker Image: validate tag existance"
+function Read-DockerImageTags {
+    param (
+        $Name,
+        $Next = "https://hub.docker.com/v2/repositories/$($Name)/tags/" 
+    )
+
+    try {
+        do {
+            $response = Invoke-RestMethod $next;
+            $response.results |
+                % { $_.name }
+
+            $next = $response.next
+        } while ($next)
+    } catch {
+        if ($_.Exception.Response.StatusCode.Value__ -ne 404) {
+            throw
+        }
+    }
+}
+$matchedTag = Get-DockerImageTags $Name |
+    ? { $Tag -contains $_ } |
+    Select-Object -First 1
+if ($matchedTag) {
+    Write-Error "Docker Image: Tag '$($matchedTag)' exists for image '$($Name)'."
+}
+
+Write-Verbose "Docker Image: tag"
+$publishTag = $Tag |
+    % {
+        $newTag = "$($Name):$($_)"
+        $arguments = @(
+            'tag', $Name, $newTag
+        )
+        & docker $arguments |
+            Out-Default
+        if ($LASTEXITCODE) {
+            Write-Error "Docker Image: 'docker $($arguments -join ' ')' failed with status $($LASTEXITCODE)."
+        }
+
+        $newTag
+    }
+
+if ($UserName) {
+    Write-Verbose "Docker Image: login"
+    $arguments = @(
+        'login',
+        '--username', $UserName,
+        '--password'
+    )
+    & docker ($arguments + $UserPassword) |
+        Out-Default
+    if ($LASTEXITCODE) {
+        Write-Error "Docker Image: 'docker $($arguments -join ' ') ***' failed with status $($LASTEXITCODE)."
+    }
+}
+
+Write-Verbose "Docker Image: push"
+$publishTag |
+    % {
+        $arguments = @(
+            'push', $_
+        )
+        & docker $arguments |
+            Out-Default
+        if ($LASTEXITCODE) {
+            Write-Error "Docker Image: 'docker $($arguments -join ' ')' failed with status $($LASTEXITCODE)."
+        }
+    }
